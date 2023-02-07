64b223f6846342d9e641f8ae443535734ba8912d
Hexi1997 <18715156450@163.com>
2023-02-07 19:24:00 +0800

    feat: add i18n

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,13 +1,34 @@
+import 'react-toastify/dist/ReactToastify.css';
 import './src/styles/global.css';
 
-import React, { ReactNode } from 'react';
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
 
 import { Layout } from '@/components/Layout';
+import { I18nextProvider } from '@/i18n';
 
 interface WrapPageElement {
-  element: ReactNode;
+  element: JSX.Element;
   props: Record<string, unknown>;
 }
+
+export const wrapRootElement = ({ element }: WrapPageElement) => {
+  return (
+    <I18nextProvider>
+      <>
+        <ToastContainer
+          autoClose={5000}
+          newestOnTop={false}
+          closeOnClick={true}
+          rtl={false}
+          draggable={false}
+          pauseOnHover={false}
+        />
+        {element}
+      </>
+    </I18nextProvider>
+  );
+};
 
 // Wraps every page in a component
 export const wrapPageElement = ({ element, props }: WrapPageElement) => {
